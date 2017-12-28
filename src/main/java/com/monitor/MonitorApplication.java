0998fd5169027a9966f2d3b3ae33983b5da9167f
jhu <hujingleo01@163.com>
2017-12-28 23:31:13 +0800

    完善结构

--- src/main/java/com/monitor/MonitorApplication.java
+++ src/main/java/com/monitor/MonitorApplication.java
@@ -1,9 +1,16 @@
 package com.monitor;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * Created by leo on 2017/12/28.
+ */
 @SpringBootApplication
+@EnableTransactionManagement    //开启事务注解
+@MapperScan("com.monitor.dao")
 public class MonitorApplication {
 
 	public static void main(String[] args) {
