c4ec920797a22030a622a1f4d8056ffbf655f4ad
胡靖 <Administrator@SC-201707191144>
2017-12-29 18:17:54 +0800

    完善定时任务

--- src/main/java/com/monitor/controller/AdminController.java
+++ src/main/java/com/monitor/controller/AdminController.java
@@ -1,56 +1,53 @@
 package com.monitor.controller;
 
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.monitor.model.Tomcatlog;
 import com.monitor.service.TomcatlogService;
 import com.monitor.utils.IoUtil;
 import com.monitor.utils.SimpleNetObject;
 
-
-
-
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-    
-	
-    @Autowired
-    private TomcatlogService tomcatlogService;
+	 private final Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private TomcatlogService tomcatlogService;
 
-    @RequestMapping("/copytomcatloglastline")
+	@RequestMapping("/copytomcatloglastline")
 	public SimpleNetObject copytomcatloglastline(String readfilepath, String writefilepath, long lines) {
 		SimpleNetObject sno = new SimpleNetObject();
 
 		if (lines == 0) {
 			lines = 1000;
 		}
-		if (null==readfilepath) {
+		if (null == readfilepath) {
 			SimpleDateFormat sfDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			String date =sfDateFormat.format(new Date());
-			readfilepath= "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/localhost_access_log."+date+".txt";
+			String date = sfDateFormat.format(new Date());
+			readfilepath = "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/localhost_access_log." + date + ".txt";
 		}
-		if (null==writefilepath) {
+		if (null == writefilepath) {
 			SimpleDateFormat sfDateFormat = new SimpleDateFormat("yyyyMMddhhmmss");
-			String date =sfDateFormat.format(new Date());
-			writefilepath = "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/accesslog."+date+".txt";
+			String date = sfDateFormat.format(new Date());
+			writefilepath = "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/accesslog." + date + ".txt";
 		}
 		int totallines = 0;
 		try {
 			totallines = IoUtil.countLines(readfilepath);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		if (totallines < lines) {
-			// return new SimpleNetObject(99,"文件总行数小于所需行数,请重新输入");
 			lines = totallines;
 		}
 		try {
@@ -58,17 +55,33 @@ public class AdminController {
 			Thread.sleep(2000);
 			tomcatlogService.copylog(writefilepath);
 			sno.setMessage("转存日志耗时" + time);
+			logger.info("info test");
+			logger.debug("debug test");
+			logger.warn("warn test");
+			logger.error("error test");
 			return sno;
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 			return new SimpleNetObject(99, "转存异常");
 		}
 	}
-//    public static void main(String[] args) {
-//		SimpleDateFormat sfDateFormat = new SimpleDateFormat("yyyyMMddhhmmss");
-//		String date =sfDateFormat.format(new Date());
-//		String writefilepath = "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/accesslog."+date+".txt";
-//		System.out.println(writefilepath);
-//	}
+//	 public static void main(String[] args) {
+//	 SimpleDateFormat sfDateFormat = new SimpleDateFormat("yyyyMMddhhmmss");
+//	 String date =sfDateFormat.format(new Date());
+//	 String writefilepath =
+//	 "/usr/software/ymall/test/apache-tomcat-7.0.53/logs/accesslog."+date+".txt";
+//	 System.out.println(writefilepath);
+//	 }
+	@RequestMapping("/testtomcatlog")
+	public SimpleNetObject testtomcatlog() {
+		SimpleNetObject sno = new SimpleNetObject();
+		List<Tomcatlog> list = tomcatlogService.testget();
+		sno.setData(list);
+		logger.info("info test");
+		logger.debug("debug test");
+		logger.warn("warn test");
+		logger.error("error test");
+		return sno;
+
+	}
 }
---
+++ src/main/java/com/monitor/utils/Task.java
@@ -0,0 +1,79 @@
+package com.monitor.utils;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.monitor.model.Tomcatlog;
+import com.monitor.service.TomcatlogService;
+
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling // 启用定时任务
+public class Task {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Value("${spring.profiles.lines}")
+	private long lines;
+	@Value("${spring.profiles.logfilepath}")
+	private String logfilepath;
+	@Value("${spring.profiles.targetfilepath}")
+	private String targetfilepath;
+	@Autowired
+	private TomcatlogService tomcatlogService;
+
+	@Scheduled(cron = "0 0/5  *  * * ? ") // 每5分钟秒执行一次
+	public void scheduler() {
+		SimpleDateFormat sfDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+		String date = sfDateFormat.format(new Date());
+		logger.info(date + "开始执行定时转存tomcat日志任务");
+		if (lines == 0) {
+			lines = 1000;
+		}
+			SimpleDateFormat sfDateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+			String date1 = sfDateFormat1.format(new Date());
+			String readfilepath = logfilepath+"/localhost_access_log." + date1 + ".txt";
+	
+			SimpleDateFormat sfDateFormat2 = new SimpleDateFormat("yyyyMMddhhmmss");
+			String date2 = sfDateFormat1.format(new Date());
+			String writefilepath = targetfilepath+"/accesslog." + date1 + ".txt";
+		int totallines = 0;
+		try {
+			totallines = IoUtil.countLines(readfilepath);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			logger.error("");
+		}
+		if (totallines < lines) {
+			lines = totallines;
+		}
+		try {
+			List<Tomcatlog> list = tomcatlogService.testget();
+			int size = list.size();
+			logger.info("info get size is "+size);
+			logger.debug("debug get size is "+size);
+			logger.warn("warn get size is "+size);
+			logger.info("get lines is "+ lines);
+			logger.info("get logfilepath is "+ logfilepath);
+			logger.info("get targetfilepath is "+ targetfilepath);
+//			long time = tomcatlogService.copytomcatloglastline(readfilepath, writefilepath, lines);
+//			Thread.sleep(2000);
+//			tomcatlogService.copylog(writefilepath);
+//			logger.info("转存日志耗时" + time);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("转存异常");
+		}
+
+	}
+}
